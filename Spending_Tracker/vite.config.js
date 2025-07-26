03c1582b51aa55e3a2bc48660632fb3d0273bf96
Kaung Myat San <kaungmyatsan@Jisoos-bfs.local>
2025-07-26 23:29:10 +0700

    Moved to root for GitHub Pages

--- Spending_Tracker/vite.config.js
+++ Spending_Tracker/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/spending_tracker/',
+  base: '/spending_tracker/Spending_Tracker/',
   plugins: [react()],
 
 })

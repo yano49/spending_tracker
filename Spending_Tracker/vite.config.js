66e1952467acd398bc5915aa98cf3a3e746df42a
Norman7781 <aungkgmyat1129@gmail.com>
2025-07-27 11:49:54 +0700

    Updated

--- Spending_Tracker/vite.config.js
+++ Spending_Tracker/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/spending_tracker/Spending_Tracker/',
+  base: '/spending_tracker/',
   plugins: [react()],
 
 })
